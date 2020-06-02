3e9f99b812ac3303902f20a69af97989a24ad377
retg96 <49927295+retg96@users.noreply.github.com>
2020-06-01 23:43:19 -0500

    Creacion de productos

--- src/controllers/v1/products-controller.js
+++ src/controllers/v1/products-controller.js
@@ -1,4 +1,21 @@
-const createProduct = (req, res)=>{};
+const Products= require ('../../mongo/models/products');
+
+const createProduct = async (req, res)=>{
+    try {
+        const{title, desc, price, images, usuarioId} = req.body;
+        const product = await Products.create({
+            title,
+            desc,
+            price,
+            images,
+            usuario: usuarioId
+        });
+        res.send({status: 'OK', data: product});
+    } catch (e) {
+        console.log('createProduct error: ', e);
+        res.status(500).send({status: 'ERROR', data: e.message});
+    }
+};
 const deleteProduct = (req, res)=>{};
 const getProducts = (req, res)=>{};
 
--- src/routes/v1/products-routes.js
+++ src/routes/v1/products-routes.js
@@ -5,4 +5,6 @@ const productsController = require ('../../controllers/v1/products-controller');
 
 const router = express.Router();
 
+router.post('/create', productsController.createProduct);
+
 module.exports=router;
